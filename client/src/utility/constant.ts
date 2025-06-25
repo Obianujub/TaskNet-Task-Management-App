3993be0c050b310bfbc9b508c0874225ea7f597b
obianujub <obianujub11@gmail.com>
2025-06-25 05:35:04 -0800

    seventh commit

--- client/src/utility/constant.ts
+++ client/src/utility/constant.ts
@@ -1,1 +1,4 @@
-export const baseUrl = "http://localhost:4000/api";
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000/api"
+    : "https://tasknet-task-management-app.onrender.com/api";
